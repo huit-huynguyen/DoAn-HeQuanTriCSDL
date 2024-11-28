5692bc524eb2c5a7267dda4111329e0fad9f60a2
Nguyen Gia Huy <vcs@z4hyrei.dev>
2024-11-28 16:57:26 +0000

    Initial works

---
+++ CuaHangThoiTrang/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CuaHangThoiTrang.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
